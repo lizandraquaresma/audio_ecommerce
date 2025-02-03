a34f3bbee8a3da2d869dfafc989390a2fdae71a8
Lizandra Quaresma <98668783+lizandraquaresma@users.noreply.github.com>
2025-02-03 09:52:10 -0300

    update: routers

--- audioecommerce/src/main.tsx
+++ audioecommerce/src/main.tsx
@@ -2,9 +2,10 @@ import ReactDOM from "react-dom/client";
 import { AuthProvider } from "./contexts/AuthContext";
 import Router from "./Router";
 import "./index.css";
+import { CartProvider } from "./contexts/CartContext";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <AuthProvider>
-    <Router />
+    <CartProvider><Router /></CartProvider>
   </AuthProvider>
 );
